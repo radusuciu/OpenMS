02dd4484c3f8a36d889e611fddc2e27545ce8af7
Radu Suciu <radusuciu@gmail.com>
2023-07-31 12:45:53 -0700

    Use logger for pyopenms warnings/errors

--- src/pyOpenMS/pyopenms/__init__.py
+++ src/pyOpenMS/pyopenms/__init__.py
@@ -21,31 +21,33 @@ Please cite:
     Proteomics. 2014 Jan;14(1):74-7. doi: 10.1002/pmic.201300246.
 
 """
-from __future__ import print_function
-
 from ._sysinfo import *  # pylint: disable=wildcard-import; lgtm(py/polluting-import)
 from ._version import version as __version__
 
+import logging
 import os
-here = os.path.abspath(os.path.dirname(__file__))
 
+logger = logging.getLogger(__name__)
+
+here = os.path.abspath(os.path.dirname(__file__))
 default_openms_data_path = os.path.join(here, "share/OpenMS")
+
 env_openms_data_path = os.environ.get("OPENMS_DATA_PATH")
 
 if os.path.exists(default_openms_data_path):
     if not env_openms_data_path:
         os.environ["OPENMS_DATA_PATH"] = default_openms_data_path
     else:
-        print(
-            "Warning: OPENMS_DATA_PATH environment variable already exists. "
+        logger.warning(
+            "OPENMS_DATA_PATH environment variable already exists. "
             "pyOpenMS will use it ({env}) to locate data in the OpenMS share folder "
             "(e.g., the unimod database), instead of the default ({default})."
             .format(env=env_openms_data_path, default=default_openms_data_path)
         )
 else:
     if not env_openms_data_path:
-         print(
-            "Warning: OPENMS_DATA_PATH environment variable not found and no share directory was installed. "
+         logger.warning(
+            "OPENMS_DATA_PATH environment variable not found and no share directory was installed. "
             "Some functionality might not work as expected."
         )
 
@@ -68,16 +70,17 @@ try:
     # export capabilities
     from ._dataframes import *  # pylint: disable=wildcard-import; lgtm(py/polluting-import)
 except Exception as e:
-    print("")
-    print("="*70)
-    print("Error when loading pyOpenMS libraries!")
-    print("Libraries could not be found / could not be loaded.")
-    print("")
-    print("To debug this error, please run ldd (on linux), otool -L (on macOS) or dependency walker (on windows) on ")
-    print("")
-    print(os.path.join(here, "pyopenms*.so"))
-    print("")
-    print("="*70)
+    logger.error("""
+======================================================================
+Error when loading pyOpenMS libraries!
+Libraries could not be found / could not be loaded.
+
+To debug this error, please run ldd (on linux), otool -L (on macOS) or dependency walker (on windows) on
+
+{pyopenms_so_path}
+
+======================================================================
+""".format(pyopenms_so_path=os.path.join(here, "pyopenms*.so")))
 
     try:
         import PyQt5.QtCore
@@ -88,22 +91,25 @@ except Exception as e:
 
         info = "\n    ".join(info.split("\n"))
 
-        print("""PyQt5 was found to be installed. When building pyopenms qmake said:
+        logger.error("""PyQt5 was found to be installed. When building pyopenms qmake said:
 
-    %s
-PYQT has version %s
+{info}
+
+PYQT has version {pyqt_version}
 
 This might cause a conflict if both are loaded.  You can test this by importing pyopenms
 first and then import PyQt5.QtCore.
-        """ % (info, PyQt5.QtCore.PYQT_VERSION_STR) )
-        
-        print("Note: when using the Spyder IDE, the usage of PyQt might be circumvented")
-        print("by not using the 'Automatic' backend. Please change this in Tools ->")
-        print("Preferences -> IPython -> Graphics to 'Inline'.")
-        print("In general, try to install everything with conda in the same environment to make sure Qt is used in the same version.")
-        print("")
-        print("="*70)
-        print("\n")
+
+Note: when using the Spyder IDE, the usage of PyQt might be circumvented
+by not using the 'Automatic' backend. Please change this in
+
+Tools -> Preferences -> IPython -> Graphics to 'Inline'
+
+In general, try to install everything with conda in the same environment to make sure Qt is used in the same version.
+
+======================================================================
+""".format(info=info, pyqt_version=PyQt5.QtCore.PYQT_VERSION_STR))
+
     raise e
 
 del os, here, sys
